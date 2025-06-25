98a57b38645a81e9f6d574792f9d79be0e50067d
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-06-25 21:59:42 +0300

    setup DOMContentLoaded

---
+++ index.js
@@ -0,0 +1,7 @@
+let showForm = false;
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const toggleFormBtn = document.querySelector("#toggleFormBtn")
+    const formContainer = document.getElementsByClassName("container")
+    const artCollection = document.getElementById("artCollection")
+})
