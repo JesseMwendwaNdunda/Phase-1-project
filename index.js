812bcb0639686be1450c469c3d3273033f1418f3
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-06-25 22:07:11 +0300

    add event listener logic for the modals

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ document.addEventListener("DOMContentLoaded", () =>{
     const modal = document.getElementById("artModal")
     const closeModalBtn = document.getElementById("closeModal")
 
+    //The function for Close modal
+    function closeModal(){
+        modal.style.display ="none"
+    }
+
+    //on button click for close modal
+    closeModalBtn.addEventListener("click",closeModal)
+
+    modal.addEventListener("click",(e)=>{
+        if(e.target ===modal){
+            closeModal()
+        }
+    })
+
 
 
 
