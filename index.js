24a60056cf2bcd064add1ce3efbce7f2d75903db
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-06-25 22:03:10 +0300

    connect modal HTML elemennts

--- index.js
+++ index.js
@@ -4,4 +4,59 @@ document.addEventListener("DOMContentLoaded", () =>{
     const toggleFormBtn = document.querySelector("#toggleFormBtn")
     const formContainer = document.getElementsByClassName("container")
     const artCollection = document.getElementById("artCollection")
+
+    //Getting the modal elements from the HTML file
+    const modal = document.getElementById("artModal")
+    const closeModalBtn = document.getElementById("closeModal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
